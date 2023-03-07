2a0763b92c9da7cab0d8cfa746004795dcdf9596
Reginara Gomes <refagomes@gmail.com>
2023-03-07 12:29:46 -0300

    First commit

---
+++ client/src/components/homeComponents/CalltoActionSection.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const CalltoActionSection = () => {
+  return (
+    <div className="subscribe-section bg-with-black">
+      <div className="container">
+        <div className="row">
+          <div className="col-xs-12">
+            <div className="subscribe-head">
+              <h2>Get styled right now</h2>
+              <p>Buy the latest in the world fashion and be fashioned too!</p>
+              <form className="form-section">
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CalltoActionSection;
